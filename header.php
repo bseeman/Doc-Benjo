b5b1dec04155b0ee1776ebfd9fdb557f0a3b3014
bseeman <benjamin.seeman@gmail.com>
2018-02-05 11:14:23 -0800

    theme pages

---
+++ header.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+
+ * The Header for our theme.
+
+ *
+
+ * Displays all of the <head> section and everything up till <div id="content">
+
+ *
+
+ * @package SKT Gravida
+
+ */
+
+?><!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+
+
+
+</head>
+
+
+
+<body <?php body_class(''); ?>>
+
+	<div id="wrapper">
+
+    	<div class="header">
+
+        		<div class="site-aligner">
+
+                	<div class="logo">
+
+                            	<h2><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h2>
+
+                                <p><?php bloginfo('description'); ?></p>
+
+                    </div><!-- logo -->
+
+                    <div class="mobile_nav"><a href="#"><?php _e('Go To...','gravida'); ?></a></div>
+
+                    <div class="site-nav">
+
+                    		<?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+
+                    </div><!-- site-nav --><div class="clear"></div>
+
+                </div><!-- site-aligner -->
+
+        </div><!-- header -->
+
+		<?php if ( is_home() || is_front_page() ) {?>
+
+       
+
+        <?php } ?>
+
+		
---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+
+	Template Name: Home
+
+ */
+
+
+
+get_header(); ?>
+
+
+
+	<div id="content">
+
+    		<div class="site-aligner">
+
+            	THIS IS FROM THE TEMPLATE
+
+            </div><!-- site-aligner -->
+
+    </div><!-- content -->
+
+<?php get_footer(); ?>
